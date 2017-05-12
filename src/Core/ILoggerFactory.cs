f08edd15c18575ecf62b278437dba584c058c443
Dmitry Shechtman <damnian@gmail.com>
2017-05-12 19:01:08 +0300

    Add solution

---
+++ src/Core/ILoggerFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Chimp.Logging
+{
+    /// <summary>
+    /// Logger factory interface.
+    /// </summary>
+    public interface ILoggerFactory
+    {
+        /// <summary>
+        /// Creates a logger for a given name.
+        /// </summary>
+        /// <param name="name">Logger name.</param>
+        /// <returns>Logger.</returns>
+        ILogger CreateLogger(string name);
+
+        /// <summary>
+        /// Creates a logger for a given type.
+        /// </summary>
+        /// <param name="type">Logger type.</param>
+        /// <returns>Logger.</returns>
+        ILogger CreateLogger(Type type);
+
+        /// <summary>
+        /// Creates a logger for a given type.
+        /// </summary>
+        /// <typeparam name="T">Logger type.</typeparam>
+        /// <returns>Logger.</returns>
+        ILogger CreateLogger<T>();
+    }
+}
---
+++ src/Core/NoOpLogger.cs
@@ -0,0 +1,40 @@
+﻿namespace Chimp.Logging
+{
+    /// <summary>
+    /// No-op logger implementation.
+    /// </summary>
+    public sealed class NoOpLogger : ILogger
+    {
+        #region Singleton
+
+        /// <summary>
+        /// No-op logger instance.
+        /// </summary>
+        public static readonly NoOpLogger Instance = new NoOpLogger();
+
+        private NoOpLogger()
+        {
+        }
+
+        #endregion
+
+        #region ILogger Members
+
+        void ILogger.Log(LogLevel level, object obj)
+        {
+            //Do nothing
+        }
+
+        void ILogger.Log(LogLevel level, string str)
+        {
+            //Do nothing
+        }
+
+        void ILogger.Log(LogLevel level, string format, object[] args)
+        {
+            //Do nothing
+        }
+
+        #endregion
+    }
+}
