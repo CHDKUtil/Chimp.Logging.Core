81f11447d3d32e7e8f644b3552d25d6067f532fd
Dmitry Shechtman <damnian@gmail.com>
2017-05-13 18:45:41 +0300

    Add NoOpLoggerFactory

---
+++ src/Core/NoOpLoggerFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Chimp.Logging
+{
+    /// <summary>
+    /// No-op logger factory implementation.
+    /// </summary>
+    public sealed class NoOpLoggerFactory : ILoggerFactory
+    {
+        #region Singleton
+
+        /// <summary>
+        /// No-op logger instance.
+        /// </summary>
+        public static readonly NoOpLoggerFactory Instance = new NoOpLoggerFactory();
+
+        private NoOpLoggerFactory()
+        {
+        }
+
+        #endregion
+
+        #region ILoggerFactory Members
+
+        ILogger ILoggerFactory.CreateLogger(string name)
+        {
+            return NoOpLogger.Instance;
+        }
+
+        ILogger ILoggerFactory.CreateLogger(Type type)
+        {
+            return NoOpLogger.Instance;
+        }
+
+        ILogger ILoggerFactory.CreateLogger<T>()
+        {
+            return NoOpLogger.Instance;
+        }
+
+        #endregion
+    }
+}
